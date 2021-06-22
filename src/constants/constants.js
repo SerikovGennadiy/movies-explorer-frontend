4ef28c0709f439ed724c3f171df73ba406135824
SerikovGennadiy <serikov.gen@yandex.ru>
2021-06-22 15:34:51 +0300

    fix: saved movie filter

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,11 +3,7 @@ export const VIEWPORT_MIDDLE = 2;
 export const VIEWPORT_THIN = 3;
 
 export const apiConfig = {
-<<<<<<< HEAD
   baseUrl : 'https://api.diploma.serikov.nomoredomains.monster',//'http://localhost:3000',//
-=======
-  baseUrl : 'http://localhost:3000',//'https://api.diploma.serikov.nomoredomains.monster'
->>>>>>> f92689ed39c2e1b7f37284fbff36146d5458728a
   movieUrl : 'https://api.nomoreparties.co/beatfilm-movies',
   headers : {
     'Content-Type' : 'application/json'
